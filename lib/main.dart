fd20dffabcdf7cd31c0a953de7e16c793fba0291
samsolon <ssolon@usa.net>
2022-05-09 17:00:56 -0700

    Counter is gone cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -66,7 +66,6 @@ class TimerNotifier extends StateNotifier<TimerState> with UiLoggy {
   _currentState() => TimerState(current: current, status: status);
 }
 
-final counterProvider = StateProvider<int>((ref) => 0);
 final timerNotifierProvider =
     StateNotifierProvider<TimerNotifier, TimerState>((ref) => TimerNotifier());
 
@@ -79,15 +78,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Workout Timer',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Workout Timer'),
@@ -111,21 +101,11 @@ class MyHomePage extends ConsumerWidget with UiLoggy {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
           // Column is also a layout widget. It takes a list of children and
           // arranges them vertically. By default, it sizes itself to fit its
@@ -143,7 +123,7 @@ class MyHomePage extends ConsumerWidget with UiLoggy {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: const <Widget>[
-            CounterDisplayWidget(),
+            TimerDisplayWidget(),
           ],
         ),
       ),
@@ -178,8 +158,8 @@ class MyHomePage extends ConsumerWidget with UiLoggy {
   }
 }
 
-class CounterDisplayWidget extends ConsumerWidget {
-  const CounterDisplayWidget({Key? key}) : super(key: key);
+class TimerDisplayWidget extends ConsumerWidget {
+  const TimerDisplayWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
